02f090cca116b4283b2a5f969e538b4ba69bc1fa
Gorilla Sapiens <embracetheape@gmail.com>
2025-09-01 13:31:26 -0700

    smarter

--- parse.pl
+++ parse.pl
@@ -1,19 +1,27 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
+
 while (<>) {
    s/^[\s]*//g;
    s/[\x0a\x0d]//g;
    s/[\s]+/ /g;
 
-   @parts = split / /;
+   my @parts = split / /;
 
    if ($parts[0] =~ /^[0-9]+$/) {
-      $ra = $parts[1];
-      $dec = $parts[2];
-      $bright = $parts[6];
-      $name = $parts[7];
+      my $ra = $parts[1];
+      my $dec = $parts[2];
+      my $bright = $parts[6];
+      my $name = $parts[0];
+
+      my ($d, $h, $m, $s, $neg);
 
       ($h,$m,$s) = split /:/, $ra;
+      if (!length($h) || !length($m) || !length($s)) {
+         next;
+      }
       $ra = int(($h * 3600 + $m * 60 + $s) * 360000000 / 86400);
 
       if ($dec =~ /^-/) {
@@ -24,6 +32,9 @@ while (<>) {
       }
       $dec =~ s/^[-\+]//g;
       ($d,$m,$s) = split /:/, $dec;
+      if (!length($d) || !length($m) || !length($s)) {
+         next;
+      }
       $dec = int (($d * 3600 + $m * 60 + $s) * 360000000 / 1296000);
       if ($neg == 1) {
          $dec = -$dec;
