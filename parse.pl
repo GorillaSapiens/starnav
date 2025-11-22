dfa163066ef9f7ff7506c5f72936e3901c324448
Gorilla Sapiens <embracetheape@gmail.com>
2025-11-21 22:46:52 -0700

    switch to sidereal lat/lon

--- parse.pl
+++ parse.pl
@@ -4,30 +4,90 @@ use strict;
 use warnings;
 
 # see https://www.johnpratt.com/items/astronomy/mag_5_stars.html
-#0  1    2  3 4   5   6    7   8  9   10 1112  13 14 15   16  17  18      19   20  21       22 23      24      25      26
-#HR,Name,ID,#,Ltr,Dbl,Con#,Con,RA,RAm,RAs,,Dec,Dm,Ds,Vmag,B-V,U-B,Sp Type,pmRA,pmD,Distance,RV,Sid Lat,Sid Lon,Zod lon,Zod
+# see https://johnpratt.com/items/astronomy/mag_5_stars.html
+
+# A	Bright Star (HR) Number
+# B	Star Name
+# C	Star Designation
+# D	Flamsteed Number
+# E	Letter Designation
+# F	Double Star Component
+# G	Constellation Number
+# H	Constellation Name
+# I	2000 R.A. (hours)
+# J	2000 R.A. (minutes)
+# K	2000 R.A. (seconds)
+# L	Declination (North or South)
+# M	Declination (°)
+# N	Declination (')
+# O	Declination (")
+# P	Visual Magnitude
+# Q	B-V Color Index
+# R	U-B Color Index
+# S	Spectral Classification
+# T	Proper Motion (R.A.)
+# U	Proper Motion (Dec.)
+# V	Distance (light years)
+# W	Radial Velocity (km/sec)
+# X	Sidereal Latitude
+# Y	Sidereal Longitude
+# Z	Zodiac Longitude
+# AA	Zodiac Sector
+
+sub debur($) {
+   my $arg = shift @_;
+   $arg =~ s/\"//g;
+   $arg =~ s/,/%2c/g;
+   return ",$arg";
+}
+
+my @abbrevs;
 my $count = 0;
 while (<>) {
-   if ($count > 0) {
-      s/^[\s]*//g;
-      s/[\x0a\x0d]//g;
-      s/[\s]+/ /g;
 
-      my @parts = split /,/;
+   s/,(\"[^\"]*\")/debur($1)/ge;
+   s/^[\s]*//g;
+   s/[\x0a\x0d]//g;
+   s/[\s]+/ /g;
+
+   my @parts = split /,/;
+   my %current;
+
+   if ($count > 0) {
+      if ($#parts != $#abbrevs) {
+         print STDERR "ERROR!!!\n";
+         print STDERR $_ . "\n";
+         exit 0;
+      }
+
+      for (my $i = 0; $i <= $#parts; $i++) {
+         $current{$abbrevs[$i]} = $parts[$i];
+      }
 
       if ($parts[0] =~ /^[0-9]+$/) {
-         my $bright = $parts[15];
-         my $name = length($parts[1]) ? $parts[1] : $parts[2];
-         $name .= " / HR $parts[0]";
+         my $bright = $current{"Vmag"};
 
-         my $ra = int(($parts[8] * 3600 + $parts[9] * 60 + $parts[10]) * 360000000 / 86400);
-         my $dec = int (($parts[12] * 3600 + $parts[13] * 60 + $parts[14]) * 360000000 / 1296000);
-         if ($parts[11] eq "S") {
-            $dec = -$dec;
-         }
+         my $name = length($current{"Name"}) ?
+            $current{"Name"} : $current{"ID"};
+         $name .= " / HR " . $current{"HR"};
 
-         print "$ra,$dec,$bright,$name\n";
+         my $lat = int($current{"Sid Lat"} * 1e6);
+
+         my $lon = int($current{"Sid Lon"} * 1e6);
+
+         print "$lon,$lat,$bright,$name\n";
       }
    }
+   else {
+      @abbrevs = @parts;
+
+      # NS for Dec is nameless???
+      for (my $i = 0; $i <= $#abbrevs; $i++) {
+         if (!length($abbrevs[$i])) {
+            $abbrevs[$i] = "Dns";
+         }
+      }
+   }
+
    $count++;
 }
